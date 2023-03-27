fe198fc80142c83452af42e7b22180158de45670
IstkharA <istkhar.ali@qbslearning.com>
2023-03-27 15:14:12 +0530

    update cors

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ let socketList = {};
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
